5188542b3dda4fe39d3afee6e6a93ad16b28be37
Lombardo Kevin <kevinlombardo30@gmail.com>
2024-07-17 13:51:49 +0200

    ajoutes de divers commentaires pour un meilleur repérage

--- src/components/AppartementContent.jsx
+++ src/components/AppartementContent.jsx
@@ -1,16 +1,25 @@
-import Carrousel from "./Carrousel";
+import Carrousel from "./Carrousel"; // import du composant carrousel
+/* Import de fonctionnalitées  */
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+/* Import des etoiles en .PNG  */
+import starFull from "../assets/Images/star-full.png";
+import starVoid from "../assets/Images/star-void.png";
+import Error404 from "../pages/Error404";
 
+// Composant  qui s'affiche quand on clique sur l'image d'un appartement dans la page HOME avec les data du fichier logement.json en paramètres;
 export default function AppartementContent({ data }) {
-  const { id } = useParams();
-  console.log(id);
-  console.log(data);
+  const { id } = useParams(); //Récupération de l'ID via la déstructuration de la pair clef/valeur retourné par useParams();
   const [appartement, setAppartement] = useState(null);
 
+  /* utilisation de useEffect pour relancer l'effet si il y a une modification de id ou data */
   useEffect(() => {
+    //logique encapsulée dans fonction pour meilleur réutilisabilité;
     const getAppartementData = () => {
+      //Utilisation de la methode find() pour trouver dans les data l'ID des données qui correspond a l'ID de l'URL
       const clickedAppartement = data.find((app) => app.id === id);
+
+      //Condition qui met a jour la variable appartement, si clickedAppartement == true (si un id correspondant a l'ID de la page a été trouvé dans data);
       if (clickedAppartement) {
         setAppartement(clickedAppartement);
       } else {
@@ -19,13 +28,19 @@ export default function AppartementContent({ data }) {
     };
 
     getAppartementData();
-  }, [id, data]);
+  }, [id, data]); //L'effet est déclenché chaque fois que "id" ou "data" change;
 
+  /* Si appartement = falsy, alors on affiche la page error404 via son composant */
   if (!appartement) {
-    return <div>Loading...</div>;
+    return <Error404 />;
   }
 
-  const tags = appartement.tags;
+  const rating = parseInt(appartement.rating);
+  const ratingArray = Array.from({ length: 5 }, (_, index) =>
+    index < rating ? starFull : starVoid
+  );
+
+  const { tags } = appartement;
 
   return (
     <div className="appartement">
@@ -59,7 +74,13 @@ export default function AppartementContent({ data }) {
               </div>
             ))}
           </div>
-          <div className="appartement-helper-rate"></div>
+          <div className="appartement-contents-helper-rating">
+            {ratingArray.map((star, index) => (
+              <div key={star}>
+                <img key={index} src={star} alt="star" />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
